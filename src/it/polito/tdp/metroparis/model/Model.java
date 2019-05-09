3d59c67eef59cc1bb6d59546483d3fc663f04c20
omar.panebianco <omar.panebianco@192.168.0.100>
2019-05-09 13:55:45 +0200

    Implementato algoritmo di visita.

--- src/it/polito/tdp/metroparis/model/Model.java
+++ src/it/polito/tdp/metroparis/model/Model.java
@@ -1,22 +1,62 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleDirectedGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+import org.jgrapht.traverse.DepthFirstIterator;
+import org.jgrapht.traverse.GraphIterator;
 
 import it.polito.tdp.metroparis.db.MetroDAO;
 
 public class Model {
 	
+	private class EdgeTraversedGraphListener implements TraversalListener<Fermata, DefaultEdge> {
+
+		@Override
+		public void connectedComponentFinished(ConnectedComponentTraversalEvent arg0) {			
+		}
+
+		@Override
+		public void connectedComponentStarted(ConnectedComponentTraversalEvent arg0) {			
+		}
+
+		@Override
+		public void edgeTraversed(EdgeTraversalEvent<DefaultEdge> e) {
+			Fermata sourceVertex = grafo.getEdgeSource(e.getEdge());
+			Fermata targetVertex = grafo.getEdgeTarget(e.getEdge());
+			
+			if(!backVisit.containsKey(targetVertex) && backVisit.containsKey(sourceVertex))
+				backVisit.put(targetVertex, sourceVertex);
+			else if(!backVisit.containsKey(sourceVertex) && backVisit.containsKey(targetVertex)) 
+				backVisit.put(sourceVertex, targetVertex);
+		}
+
+		@Override
+		public void vertexFinished(VertexTraversalEvent<Fermata> arg0) {			
+		}
+
+		@Override
+		public void vertexTraversed(VertexTraversalEvent<Fermata> arg0) {			
+		}
+	}
+	
 	private MetroDAO dao = new MetroDAO();
 	private Graph<Fermata, DefaultEdge> grafo;
 	private List<Fermata> fermate;
 	private Map<Integer, Fermata> fermateIdMap;
+	Map<Fermata, Fermata> backVisit;
 	
 	public void creaGrafo() {
 		// Creo l'oggetto grafo
@@ -51,6 +91,43 @@ public class Model {
 		/* Aggiungo gli archi (opzione 3)
 		 * utilizzo in modo diretto le colonne id_stazP e id_stazA della tabella connessione
 		 */
+	}
+	
+	public List<Fermata> fermateRaggiungibili(Fermata source) {
+		List<Fermata> result = new ArrayList<>();
+		backVisit = new HashMap<>();
+		
+		GraphIterator<Fermata, DefaultEdge> it = new BreadthFirstIterator<>(this.grafo, source);
+		// GraphIterator<Fermata, DefaultEdge> it = new DepthFirstIterator<>(this.grafo, source);
+		
+		it.addTraversalListener(new Model.EdgeTraversedGraphListener());
+		
+		backVisit.put(source, null);
+		
+		while(it.hasNext()) {
+			result.add(it.next());
+		}
+		
+		// System.out.println(backVisit);
+		
+		return result;
+	}
+	
+	public List<Fermata> percorsoFinoA(Fermata target) {
+		if(!backVisit.containsKey(target)) {
+			// target non raggiungibile dalla source
+			return null;
+		}
+		
+		List<Fermata> percorso = new LinkedList<>();
+		Fermata f = target;
+		
+		while(f != null) {
+			percorso.add(0, f);
+			f = backVisit.get(f);
+		}
+		
+		return percorso;
 	}
 
 	public Graph<Fermata, DefaultEdge> getGrafo() {
--- src/it/polito/tdp/metroparis/model/TestModel.java
+++ src/it/polito/tdp/metroparis/model/TestModel.java
@@ -1,5 +1,7 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.List;
+
 public class TestModel {
 
 	public static void main(String[] args) {
@@ -8,8 +10,18 @@ public class TestModel {
 		
 		model.creaGrafo();
 		
-		System.out.println(model.getGrafo());
+		// System.out.println(model.getGrafo());
+		System.out.format("Creati %d vertici e %d archi\n", model.getGrafo().vertexSet().size(), model.getGrafo().edgeSet().size());
 		
+		Fermata source = model.getFermate().get(0);
+		System.out.println("Parto da: " + source);
+		List<Fermata> raggiungibili = model.fermateRaggiungibili(source);
+		System.out.println("Fermate raggiunte: " + raggiungibili + "\ndim: " + raggiungibili.size());
+		
+		Fermata target = model.getFermate().get(150);
+		List<Fermata> percorso = model.percorsoFinoA(target);
+		System.out.println("Arrivo a: " + target);
+		System.out.println(percorso);
 	}
 
 }
